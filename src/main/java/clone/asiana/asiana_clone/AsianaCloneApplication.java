559df14ad5574d933bf9d1aa3fdc808949b9effd
YooHyeonJun <116273485+Hyeonjun405@users.noreply.github.com>
2025-10-12 22:25:59 +0900

    스프링부트 초기 데이터 커밋

---
+++ src/main/java/clone/asiana/asiana_clone/AsianaCloneApplication.java
@@ -0,0 +1,13 @@
+package clone.asiana.asiana_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsianaCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsianaCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clone/asiana/asiana_clone/AsianaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package clone.asiana.asiana_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsianaCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
