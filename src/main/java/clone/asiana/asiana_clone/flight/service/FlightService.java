f7672e8dcdaf600de79a6dfb9fc97979469db248
Hyeonjun405 <youhj14@naver.com>
2025-10-19 22:33:39 +0900

    마이바티스용 로직 추가

--- src/main/java/clone/asiana/asiana_clone/flight/service/FlightService.java
+++ src/main/java/clone/asiana/asiana_clone/flight/service/FlightService.java
@@ -1,7 +1,11 @@
 package clone.asiana.asiana_clone.flight.service;
 
 
+import clone.asiana.asiana_clone.flight.vo.FindFlight;
 import clone.asiana.asiana_clone.flight.vo.FlightStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -11,27 +15,43 @@ import java.util.List;
 
 
 @Service
+@Slf4j
 public class FlightService {
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;
 
 
     public List<String> findAllDepartureAirports(){
 
-        return Arrays.asList("ICN", "GMP", "PUS", "CJU");
+        return Arrays.asList(
+                "ICN", "GMP", "PUS", "CJU", "HND"
+        );
     };
 
     public List<String> findAllArrivalAirports(){
 
-        return Arrays.asList("HND", "NRT", "FUK", "CTS", "KIX");
+        return Arrays.asList(
+                "ICN", "GMP", "PUS", "CJU", "HND"
+        );
     };
 
-    public List<FlightStatus> findFlights(String from, String to, LocalDate departureDate){
+    public List<FlightStatus> findFlights(FindFlight findFlight){
 
-        List<FlightStatus> flights = new ArrayList<>();
-        flights.add(new FlightStatus("KE123", "ICN", "HND", "2025-11-01 08:00", "2025-11-01 10:00", "Scheduled"));
-        flights.add(new FlightStatus("OZ456", "ICN", "NRT", "2025-11-01 09:30", "2025-11-01 11:45", "Delayed"));
-        flights.add(new FlightStatus("JL789", "ICN", "FUK", "2025-11-01 07:15", "2025-11-01 09:05", "Cancelled"));
-        flights.add(new FlightStatus("KE321", "ICN", "CTS", "2025-11-01 12:00", "2025-11-01 14:30", "Scheduled"));
-        flights.add(new FlightStatus("OZ654", "ICN", "KIX", "2025-11-01 15:20", "2025-11-01 17:10", "Scheduled"));
+
+
+//        flights.add(new FlightStatus("KE123", "ICN", "HND", "2025-11-01 08:00", "2025-11-01 10:00", "Scheduled"));
+//        flights.add(new FlightStatus("OZ456", "ICN", "NRT", "2025-11-01 09:30", "2025-11-01 11:45", "Delayed"));
+//        flights.add(new FlightStatus("JL789", "ICN", "FUK", "2025-11-01 07:15", "2025-11-01 09:05", "Cancelled"));
+//        flights.add(new FlightStatus("KE321", "ICN", "CTS", "2025-11-01 12:00", "2025-11-01 14:30", "Scheduled"));
+//        flights.add(new FlightStatus("OZ654", "ICN", "KIX", "2025-11-01 15:20", "2025-11-01 17:10", "Scheduled"));
+
+        log.info(findFlight.getDepartureAirport());
+        log.info(findFlight.getArrivalAirport());
+
+        List<FlightStatus> flights = sqlSession.selectList("FlightStatusMapper.flights_search", findFlight);
+
+
 
         return flights;
     }
---
+++ src/main/java/clone/asiana/asiana_clone/flight/vo/FindFlight.java
@@ -0,0 +1,38 @@
+package clone.asiana.asiana_clone.flight.vo;
+
+public class FindFlight {
+
+    private String departureAirport;
+    private String arrivalAirport;
+    private String departureTime;
+
+    public FindFlight(String departureAirport, String arrivalAirport, String departureTime) {
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.departureTime = departureTime;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+}
