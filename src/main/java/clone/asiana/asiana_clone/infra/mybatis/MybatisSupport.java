9e9884416710064ce022d7503eb4f74ebaea16a2
Hyeonjun405 <youhj14@naver.com>
2025-10-19 22:33:02 +0900

    마이바티스 추가

---
+++ src/main/java/clone/asiana/asiana_clone/infra/mybatis/MybatisSupport.java
@@ -0,0 +1,69 @@
+package clone.asiana.asiana_clone.infra.mybatis;
+
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Matcher;
+
+@Intercepts({
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "query",  args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})
+})
+public class MybatisSupport implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+
+        System.out.println("인터셉터 시작");
+
+        MappedStatement ms = (MappedStatement) invocation.getArgs()[0];
+        Object parameterObject = invocation.getArgs().length > 1 ? invocation.getArgs()[1] : null;
+
+        BoundSql boundSql = ms.getBoundSql(parameterObject);
+        String sql = boundSql.getSql().replaceAll("\\s+", " "); // 공백 정리
+
+        // 실제 파라미터 값 바인딩
+        Configuration configuration = ms.getConfiguration();
+        List<ParameterMapping> paramMappings = boundSql.getParameterMappings();
+        if (paramMappings != null && !paramMappings.isEmpty()) {
+            MetaObject metaObject = configuration.newMetaObject(parameterObject);
+
+            for (ParameterMapping mapping : paramMappings) {
+                String propertyName = mapping.getProperty();
+                if (metaObject.hasGetter(propertyName)) {
+                    Object value = metaObject.getValue(propertyName);
+                    String valueStr = (value == null) ? "NULL" : "'" + value.toString() + "'";
+                    sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(valueStr));
+                }
+            }
+        }
+
+        System.out.println("실행 SQL: " + sql);
+        System.out.println("인터셉터 끝");
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Interceptor.super.plugin(target);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        Interceptor.super.setProperties(properties);
+    }
+}
