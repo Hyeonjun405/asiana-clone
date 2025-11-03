55d45a788de7be1b7379eec05ea3060375c6918d
Hyeonjun405 <youhj14@naver.com>
2025-11-03 18:00:47 +0900

    Exception 제거

--- src/main/java/clone/asiana/asiana_clone/login/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package clone.asiana.asiana_clone.login.exception;
-
-public class AuthenticationException extends RuntimeException{
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/clone/asiana/asiana_clone/login/exception/OtpException.java
+++
@@ -1,7 +0,0 @@
-package clone.asiana.asiana_clone.login.exception;
-
-public class OtpException extends RuntimeException{
-    public OtpException(String message) {
-        super(message);
-    }
-}
