94bf9ce5584f081a93b09457af55276f8f8d4a01
Hyeonjun405 <youhj14@naver.com>
2025-11-03 18:02:15 +0900

    마이바티스 인터셉터 조정

--- src/main/java/clone/asiana/asiana_clone/infra/mybatis/MybatisSqlSupport.java
+++ src/main/java/clone/asiana/asiana_clone/infra/mybatis/MybatisSqlSupport.java
@@ -3,12 +3,20 @@ package clone.asiana.asiana_clone.infra.mybatis;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
+import java.util.regex.Matcher;
 
 @Intercepts({
         @Signature(type = Executor.class,
@@ -31,9 +39,11 @@ public class MybatisSqlSupport implements Interceptor {
             parameter = invocation.getArgs()[1];
         }
 
-        // BoundSql 가져오기
-        org.apache.ibatis.mapping.BoundSql boundSql = ms.getBoundSql(parameter);
-        String sql = boundSql.getSql().replaceAll("\\s+", " "); // 공백 정리
+        BoundSql boundSql = ms.getBoundSql(parameter);
+        Configuration configuration = ms.getConfiguration();
+
+        // 실제 파라미터가 치환된 SQL 생성
+        String sql = getCompleteSql(boundSql, parameter, configuration);
 
         // ThreadLocal 기반 중복 필터
         String currentId = ms.getId();
@@ -55,5 +65,39 @@ public class MybatisSqlSupport implements Interceptor {
 
     @Override
     public void setProperties(Properties properties) {
+    }
+
+    // BoundSql + Parameter 치환 함수
+    private String getCompleteSql(BoundSql boundSql, Object parameterObject, Configuration configuration) {
+        String sql = boundSql.getSql().replaceAll("\\s+", " ");
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+
+        if (parameterMappings != null) {
+            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+            for (ParameterMapping mapping : parameterMappings) {
+                String propertyName = mapping.getProperty();
+                Object value;
+
+                if (boundSql.hasAdditionalParameter(propertyName)) { // 동적 파라미터
+                    value = boundSql.getAdditionalParameter(propertyName);
+                } else if (parameterObject == null) {
+                    value = null;
+                } else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                    value = parameterObject;
+                } else {
+                    MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                    value = metaObject.getValue(propertyName);
+                }
+
+                sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(formatParameter(value)));
+            }
+        }
+        return sql;
+    }
+
+    private String formatParameter(Object obj) {
+        if (obj == null) return "NULL";
+        if (obj instanceof String || obj instanceof Date) return "'" + obj.toString() + "'";
+        return obj.toString();
     }
 }
