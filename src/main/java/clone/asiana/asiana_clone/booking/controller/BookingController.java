6ba7488591b110a1fa15048e0cf46763b4c36283
Hyeonjun405 <youhj14@naver.com>
2025-11-14 17:56:22 +0900

    결제페이지 생성

--- src/main/java/clone/asiana/asiana_clone/booking/controller/BookingController.java
+++ src/main/java/clone/asiana/asiana_clone/booking/controller/BookingController.java
@@ -1,12 +1,14 @@
 package clone.asiana.asiana_clone.booking.controller;
 
 
+import clone.asiana.asiana_clone.booking.dto.BookingRequestDTO;
 import clone.asiana.asiana_clone.booking.dto.BookingSearchDTO;
 import clone.asiana.asiana_clone.booking.service.BookingService;
 import clone.asiana.asiana_clone.booking.vo.SearchFlightVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +24,7 @@ public class BookingController {
     BookingService service;
 
     @GetMapping
-    public String booking() {
+    public String booking(Model model) {
         return "booking/booking";
     }
 
@@ -30,16 +32,31 @@ public class BookingController {
     public String bookingSearch(@ModelAttribute BookingSearchDTO search, RedirectAttributes redirectAttributes){
 
         SearchFlightVO outboundFlight = SearchFlightVO.fromOutbound(search);
+//        redirectAttributes.addFlashAttribute("request", search);
         redirectAttributes.addFlashAttribute("outboundFlights", service.SearchFlights(outboundFlight));
+        redirectAttributes.addFlashAttribute("tripType", "ONEWAY");
 
         if(search.getReturnDate() != null) {
             SearchFlightVO returnFlight = SearchFlightVO.fromReturn(search);
             redirectAttributes.addFlashAttribute("returnFlights", service.SearchFlights(returnFlight));
+            redirectAttributes.addFlashAttribute("tripType", "round");
         }
 
         return "redirect:/booking";
     }
 
+    @GetMapping("/payment")
+    public String payment() {
+        return "booking/payment";
+    }
+
+    @PostMapping("/payment")
+    public String payment(@ModelAttribute BookingRequestDTO param){
+
+        log.info("값 : " + param.toString());
+
+        return "redirect:/booking/payment";
+    }
 
 
 }
